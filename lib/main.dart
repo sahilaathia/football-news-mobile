4a58a31a8d0a47844d256655746f6c41e911eeb1
sahilaathia <athiasahila@gmail.com>
2025-11-05 16:38:26 +0700

    Tutorial 7: Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:football_news_mobile/menu.dart';
+import 'package:football_news_mobile/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.teal)
-        .copyWith(secondary: Colors.tealAccent[400]),
+        .copyWith(secondary: [400]Colors.tealAccent),
       ),
       home: MyHomePage(),
     );
---
+++ lib/widgets/left_drawer.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:football_news_mobile/screens/menu.dart';
+import 'package:football_news_mobile/screens/newslist_form.dart';
+
+class LeftDrawer extends StatelessWidget {
+  const LeftDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            // Bagian drawer header
+            decoration: BoxDecoration(
+              color: Colors.teal,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Football News',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text(
+                  "Seluruh berita sepak bola terkini di sini!",
+                  // Gaya teks dengan center alignment, font ukuran 15, warna putih, dan weight biasa
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white,
+                  )
+                ),
+              ],
+            ),
+          ),
+
+          // Bagian routing
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            // Bagian redirection ke MyHomePage
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(),
+                  ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.post_add),
+            title: const Text('Add News'),
+            // Bagian redirection ke NewsFormPage
+            onTap: () {
+              // Routing ke NewsFormPage
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NewsFormPage(),
+                )
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
